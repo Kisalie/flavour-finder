8b2205c1b32040f4dbbfd9c9757362f996a1c95a
developedbyluke <lukehorton7@yahoo.com>
2023-09-24 20:38:47 +0100

    rebase

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 // import axios from 'axios'
@@ -9,25 +10,39 @@ export default function Profile() {
   const { addedBy } = useParams()
   // const userId = isAuthenticated()
   // const [usersRecipes, setUsersRecipes] = useState([])
+=======
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { isAuthenticated } from '../utils/auth.js'
 
-  // useEffect(() => {
-  //   async function getRecipesByUser() {
-  //     try {
-  //       const { data } = await axios.get(`/user/${userId}`)
-  //       setUsersRecipes(data)
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
-  //   }
-  //   getRecipesByUser()
-  // }, []
-  // )
+export default function Profile() {
+  const userId = isAuthenticated()
+  const [usersRecipes, setUsersRecipes] = useState([])
+>>>>>>> ea88f2e (profile working, temp hard coded user)
+
+  useEffect(() => {
+    async function getRecipesByUser() {
+      try {
+        // const { data } = await axios.get(`/user/${userId}`)
+        const { data } = await axios.get('/api/user/650c2cfbde13d1a3da109eff')
+        setUsersRecipes(data)
+        console.log(userId)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    getRecipesByUser()
+  }, []
+
+  )
 
 
 
   return (
     <main className='user-profile'>
       <h1>Welcome back!</h1>
+<<<<<<< HEAD
       <h2>My recipes</h2>
       <Link to={`/user/${addedBy}/create`}>
         Create New Recipe
@@ -43,10 +58,25 @@ export default function Profile() {
           >
             <div className='recipe-title'>
               <p>{recipe.title}</p>
+=======
+      <h2>Recipes you added:</h2>
+      <section className='grid-container'>
+        {usersRecipes.map((recipe) => (
+          <Link key={recipe._id} to={`/recipes/${recipe._id}`} className='recipe'>
+            <div
+              className='recipe-container'
+              style={{
+                backgroundImage: `url(${recipe.image})`,
+              }}
+            >
+              <div className='recipe-title'>
+                <p>{recipe.title}</p>
+              </div>
+>>>>>>> ea88f2e (profile working, temp hard coded user)
             </div>
-          </div>
-        </Link>
-      ))} */}
+          </Link>
+        ))}
+      </section>
     </main>
   )
 }
