d982139d3369d40c0b3394be99964705f1ad804a
developedbyluke <lukehorton7@yahoo.com>
2023-09-24 20:29:42 +0100

    merge

--- db/seeds.js
+++ db/seeds.js
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 import mongoose from 'mongoose'
+=======
+import mongoose from 'mongoose' 
+>>>>>>> 2b63c61 (blogs api complete)
 import 'dotenv/config'
 
 // Model
 import Recipe from '../models/recipe.js'
 import User from '../models/user.js'
+<<<<<<< HEAD
 
 // Data
 import recipeData from './data/recipes.js'
@@ -22,6 +27,45 @@ const seedDatabase = async () => {
 
     const usersAdded = await User.create(userData)
     console.log(`➕ added ${usersAdded.length} user`)
+=======
+import Blog from '../models/blog.js'
+
+// Data
+import recipeData from './data/recipes.js' 
+import userData from './data/users.js'
+import blogData from './data/blogs.js'
+
+
+
+
+
+
+const seedDatabase = async () => {
+  try {
+
+    await mongoose.connect(process.env.CONNECTION_STRING)
+    console.log('🥙 Database connection established')
+
+
+    const { deletedCount: usersDeleted } = await User.deleteMany()
+    console.log(`❌👩‍🦰 Deleted ${usersDeleted} users`)
+
+
+    const { deletedCount: recipeDeleted } = await Recipe.deleteMany()
+    console.log(`❌🥘 Deleted ${recipeDeleted} recipes`)
+
+    const { deletedCount: blogsDeleted } = await Blog.deleteMany()
+    console.log(`❌🥘 Deleted ${blogsDeleted} blogs`)
+
+
+    const usersAdded = await User.create(userData)
+    console.log(`👩‍🦰 added ${usersAdded.length} users`)
+
+    const blogsAdded = await Blog.create(blogData)
+    console.log(`📖 added ${blogsAdded.length} blogs`)
+
+
+>>>>>>> 2b63c61 (blogs api complete)
 
     const recipeWithAddedBy = recipeData.map(recipe => {
       const randomUserId = Math.floor(Math.random() * usersAdded.length)
@@ -29,8 +73,15 @@ const seedDatabase = async () => {
     })
 
     const recipeAdded = await Recipe.create(recipeWithAddedBy)
+<<<<<<< HEAD
     console.log(`🥘 ${recipeAdded.length} recipe added to the recipe collection`)
 
+=======
+    console.log(`🥘 added ${recipeAdded.length} recipes`)
+
+
+
+>>>>>>> 2b63c61 (blogs api complete)
     await mongoose.connection.close()
     console.log('👋 connection closed')
   } catch (error) {
@@ -39,4 +90,8 @@ const seedDatabase = async () => {
     console.log('👋 connection closed')
   }
 }
+<<<<<<< HEAD
 seedDatabase()
+=======
+seedDatabase()
+>>>>>>> 2b63c61 (blogs api complete)
