41d94cb705217cb20ac2c9d4d18b318a5974350d
developedbyluke <lukehorton7@yahoo.com>
2023-09-24 20:39:44 +0100

    rebase

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -12,18 +12,28 @@ export default function Profile() {
   // const [usersRecipes, setUsersRecipes] = useState([])
 =======
 import React, { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
-import { isAuthenticated } from '../utils/auth.js'
+import { isAuthenticated, getToken } from '../utils/auth.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons'
+
+
 
 export default function Profile() {
+
   const userId = isAuthenticated()
   const [usersRecipes, setUsersRecipes] = useState([])
+<<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> ea88f2e (profile working, temp hard coded user)
 =======
   console.log(userId)
 >>>>>>> ac954f1 (changes)
+=======
+  const { id } = useParams()
+  const navigate = useNavigate()
+>>>>>>> 0d71b74 (delete draft)
 
   useEffect(() => {
     async function getRecipesByUser() {
@@ -48,6 +58,21 @@ export default function Profile() {
     getRecipesByUser()
   }, [])
 
+<<<<<<< HEAD
+=======
+  async function handleDelete(id){
+    try {
+      await axios.delete(`/api/recipes/${id}`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+      navigate('/user/:addedBy')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+>>>>>>> 0d71b74 (delete draft)
 
   return (
     <main className='user-profile'>
@@ -57,6 +82,7 @@ export default function Profile() {
       <Link to={`/user/${addedBy}/create`}>
         Create New Recipe
       </Link>
+<<<<<<< HEAD
 <<<<<<< HEAD
 
       {/* {usersRecipes.map((recipe) => (
@@ -73,6 +99,8 @@ export default function Profile() {
 =======
 >>>>>>> 1a38f65 (Updated dropdown links and added Create Recipe to login)
       <h2>Recipes you added:</h2>
+=======
+>>>>>>> 0d71b74 (delete draft)
       <section className='grid-container'>
         {usersRecipes.map((recipe) => (
           <Link key={recipe._id} to={`/recipes/${recipe._id}`} className='recipe'>
@@ -82,6 +110,14 @@ export default function Profile() {
                 backgroundImage: `url(${recipe.image})`,
               }}
             >
+              { userId === recipe.addedBy._id && 
+                <div className="edit-buttons">
+                  <FontAwesomeIcon onClick={handleDelete(id)} icon={faTrash} />
+                  <Link to={`/user/${recipe.addedBy._id}/${id}`}>
+                    <FontAwesomeIcon className='update' icon={faPen} />
+                  </Link>
+                </div>
+              }
               {/* Icon as button for delete -> onClick to delete, <Link> for update that redirects to update recipe page on click */}
               {/* CSS: position: absolute, right: 0 (Must have position: relative; on the parent recipe container) */}
               <div className='recipe-title'>
@@ -98,7 +134,11 @@ export default function Profile() {
     </main>
   )
 <<<<<<< HEAD
+<<<<<<< HEAD
 }
 =======
 }
 >>>>>>> 1a38f65 (Updated dropdown links and added Create Recipe to login)
+=======
+}
+>>>>>>> 0d71b74 (delete draft)
